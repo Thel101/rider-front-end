387569457702c00a21652ccc268b140d6e7aef12
Phyu Thet <phyumyeon@gmail.com>
2024-01-25 06:24:39 +0630

    login form

---
+++ src/views/LogIn.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <v-row>
+      <v-col cols="4">
+        <v-card>
+          <v-card-title>Log-in Form</v-card-title>
+        </v-card>
+      </v-col>
+    </v-row>
+
+  </div>
+</template>
